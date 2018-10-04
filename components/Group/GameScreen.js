54a009e95f488e0058dfe77aa2f651b08f793959
Patrick Truong <pattruong@Patricks-MBP.attlocal.net>
2018-10-03 17:38:28 -0700

    implemented pushchat

--- components/Group/GameScreen.js
+++ components/Group/GameScreen.js
@@ -1,21 +1,89 @@
-import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native'
+// import { Text, View, Image } from 'react-native'
 
-class GameScreen extends Component {
-  state = {}
+import React from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+import Chatkit from "@pusher/chatkit";
+import {
+  CHATKIT_TOKEN_PROVIDER_ENDPOINT,
+  CHATKIT_INSTANCE_LOCATOR,
+  CHATKIT_ROOM_ID,
+  CHATKIT_USER_NAME
+} from '../../config/info';
+
+
+export default class GameScreen extends React.Component {
+  state = {
+    messages: []
+  };
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('title', 'A Nested Details Screen'),
+    };
+  };
+
+
+  componentDidMount() {
+    // This will create a `tokenProvider` object. This object will be later used to make a Chatkit Manager instance.
+    const tokenProvider = new Chatkit.TokenProvider({
+      url: CHATKIT_TOKEN_PROVIDER_ENDPOINT
+    });
+
+    // This will instantiate a `chatManager` object. This object can be used to subscribe to any number of rooms and users and corresponding messages.
+    // For the purpose of this example we will use single room-user pair.
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: CHATKIT_INSTANCE_LOCATOR,
+      userId: CHATKIT_USER_NAME,
+      tokenProvider: tokenProvider
+    });
+
+    // In order to subscribe to the messages this user is receiving in this room, we need to `connect()` the `chatManager` and have a hook on `onNewMessage`. There are several other hooks that you can use for various scenarios. A comprehensive list can be found [here](https://docs.pusher.com/chatkit/reference/javascript#connection-hooks).
+    chatManager.connect().then(currentUser => {
+      this.currentUser = currentUser;
+      this.currentUser.subscribeToRoom({
+        roomId: CHATKIT_ROOM_ID,
+        hooks: {
+          onNewMessage: this.onReceive
+        }
+      });
+    });
+  }
+
+  onSend([message]) {
+    this.currentUser.sendMessage({
+      text: message.text,
+      roomId: CHATKIT_ROOM_ID
+    });
+  }
+
+  onReceive = (data) => {
+    const { id, senderId, text, createdAt } = data;
+    const incomingMessage = {
+      _id: id,
+      text: text,
+      createdAt: new Date(createdAt),
+      user: {
+        _id: senderId,
+        name: senderId,
+        avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmXGGuS_PrRhQt73sGzdZvnkQrPXvtA-9cjcPxJLhLo8rW-sVA"
+      }
+    };
+
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, incomingMessage)
+    }));
+  }
 
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Welcome to the Game Screen: {this.props.navigation.getParam('title', 'default')}</Text>
-        <Image
-          source={{ uri: this.props.navigation.getParam('img_url', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png') }}
-          style={{ height: 300, width: 300 }}
-        />
-      </View>
-    );
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: CHATKIT_USER_NAME
+        }}
+      />
+    )
   }
 }
-
-export default GameScreen;
