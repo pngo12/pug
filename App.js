a256f32f2be6c6406f6d6d9820519137836ab65a
Patrick Truong <pattruong@Patricks-MBP.attlocal.net>
2018-10-05 10:44:18 -0700

    redux laid out

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from 'react-navigation';
+
+// Imports to setup Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './components/Redux/Reducers';
 
 import GroupStack from './components/Group/GroupStack'
 import ProfileStack from './components/Profile/ProfileStack'
@@ -10,9 +15,9 @@ import SettingsStack from './components/Settings/SettingsStack'
 
 const TabNavigator = createBottomTabNavigator(
   {
-      Home: GroupStack,
-      Profile: ProfileStack,
-      Settings: SettingsStack
+    Home: GroupStack,
+    Profile: ProfileStack,
+    Settings: SettingsStack
   },
   {
     navigationOptions: ({ navigation }) => ({
@@ -37,4 +42,14 @@ const TabNavigator = createBottomTabNavigator(
   }
 );
 
-export default App = () => <TabNavigator />
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+);
+
+export default App = () => (
+  <Provider store={store}>
+    <TabNavigator />
+  </Provider>
+);
