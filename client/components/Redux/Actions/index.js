3e655ecf653600932400e880ee024ea91213050e
Patrick Truong <pattruong@gmail.com>
2018-10-14 16:43:09 -0700

    redux actions small cleanup

--- client/components/Redux/Actions/index.js
+++ client/components/Redux/Actions/index.js
@@ -11,7 +11,8 @@ import {
   SUBSCRIBE,
   CHANGE_CHATROOM,
   SEND_MESSAGE,
-  NO_JOINED_ROOMS
+  NO_JOINED_ROOMS,
+  ERROR_SUBSCRIBING
 } from '../Constants'
 
 import {
@@ -78,7 +79,7 @@ export const subscribeToAllJoined = userId => async dispatch => {
   } catch (err) {
     console.log("USER ID: ", userId)
     console.error(err);
-    dispatch({ type: "ERROR SUBSCRIBING" })
+    dispatch({ type: ERROR_SUBSCRIBING })
   }
 }
 
